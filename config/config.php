3e6250615a3a076fde358b3e65b2180a12faef55
syescassut <Sylvain.ESCASSUT@etu.uca.fr>
2017-11-17 13:31:09 +0100

    update to php7

--- config/config.php
+++ config/config.php
@@ -1,19 +1,19 @@
 <?php 	
-    //directory
-    $dir=__DIR__.'/../';
+    //Directory
+    $dir = __DIR__.'/../';
     
     //DB
-    $base="mysql:host=localhost;dbname=AFrame-CV-Default";
-    $login="root";
-    $password="root";
+    $base = "mysql:host=localhost;dbname=AFrame-CV-Default";
+    $login = "root";
+    $password = "root";
 
-	//Views
-        $views['head'] = "views/head.html";
-        $views['foot'] = "views/foot.html";
-	$views['error'] = "views/error.php";
-	$views['home'] = "views/home.php";
-	$views['loft'] = "views/3dElements/loft.php";
-	$views['door'] = "views/3dElements/doors.html";
-	$views['publication'] = "views/3dElements/publication.html";
-	$views['displayPlane'] = "views/displayPlane.php";
+    //Views
+    $views['head'] = "views/head.html";
+    $views['foot'] = "views/foot.html";
+    $views['error'] = "views/error.php";
+    $views['home'] = "views/home.php";
+    $views['loft'] = "views/3dElements/loft.php";
+    $views['door'] = "views/3dElements/doors.html";
+    $views['publication'] = "views/3dElements/publication.html";
+    $views['displayPlane'] = "views/displayPlane.php";
 ?>
