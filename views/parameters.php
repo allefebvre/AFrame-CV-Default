00545477dd8642ce2706befa6dac9391d0c716f8
Alexandre LEFEBVRE <Alexandre.LEFEBVRE@etu.uca.fr>
2017-11-24 15:37:56 +0100

    improve administration presentation

--- views/parameters.php
+++ views/parameters.php
@@ -1,40 +1,61 @@
 <aside class="parameters">
-    <div id="parametersContent">
+    <div>
         <h1>Parameters</h1>
         <h3>Display plane :</h3>
-        <table>
-            <tr>
-                <td><input id="plane1" type="checkbox"></td>
-                <td>Plane1</td>
-            </tr>
-            <tr>
-                <td><input id="plane2" type="checkbox"></td>
-                <td>Plane2</td>
-            </tr>
-            <tr>
-                <td><input id="plane3" type="checkbox"></td>
-                <td>Plane3</td>
-            </tr>
-            <tr>
-                <td><input id="plane4" type="checkbox"></td>
-                <td>Plane4</td>
-            </tr>
-            <tr>
-                <td><input id="plane5" type="checkbox"></td>
-                <td>Plane5</td>
-            </tr>
-        </table>
+
+        <ul class="listParam">
+            <li>
+                <input id="plane1" type="checkbox">
+                <div class="checkButton"></div>
+                <div class="inside"></div>
+                <div><label for="plane1">Plane1</label></div>
+            </li>
+            <li>
+                <input id="plane2" type="checkbox">
+                <div class="checkButton"></div>
+                <div class="inside"></div>
+                <div><label for="plane2">Plane2</label></div>
+            </li>
+            <li>
+                <input id="plane3" type="checkbox">
+                <div class="checkButton"></div>
+                <div class="inside"></div>
+                <div><label for="plane3">Plane3</label></div>
+            </li>
+            <li>
+                <input id="plane4" type="checkbox">
+                <div class="checkButton"></div>
+                <div class="inside"></div>
+                <div><label for="plane4">Plane4</label></div>
+            </li>
+            <li>
+                <input id="plane5" type="checkbox">
+                <div class="checkButton"></div>
+                <div class="inside"></div>
+                <div><label for="plane5">Plane5</label></div>
+            </li>
+        </ul>
+
         <h3>Display publications ?</h3>
-        <p id="pRadioInput">
-            <input type="radio" name="publications" value="yes"> Yes<br>
-            <input type="radio" name="publications" value="no"> No<br>
-        </p>
-        <p class="button-parameter">
+
+        <ul class="listParam">
+            <li>
+                <input type="radio" name="publications" id="radio_1" value="yes">
+                <div class="radioButton"></div>
+                <div class="inside"></div>
+                <div><label for="radio_1">Yes</label></div>
+            </li>
+            <li>
+                <input type="radio" name="publications" id="radio_2" value="no">
+                <div class="radioButton"></div>
+                <div class="inside"></div>
+                <div><label for="radio_2">No</label></div>
+            </li>
+        </ul>
+
+        <div class="links">
             <a id="parametersSave" href="index.php?action=saveParameters">Save</a>
-        </p>
-        <p class="button-parameter">
             <a id="addData" href="views/insertInDatabase.php">Add Data</a>
-        </p>
+        </div>
     </div>
 </aside>
-
