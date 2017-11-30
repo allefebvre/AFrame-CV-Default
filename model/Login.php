6702149241bb5e5ef6f10b7ee7145c4c1f459c8c
Alexandre LEFEBVRE <Alexandre.LEFEBVRE@etu.uca.fr>
2017-11-30 09:28:07 +0100

    improve token

--- model/Login.php
+++ model/Login.php
@@ -55,8 +55,8 @@ class Login {
         $tab = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n' , 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
         $token = "";
         $max = count($tab);
-        for ($index = 0; $index < 100; $index++) {
-            $token .= $tab[rand(0, 35)];
+        for ($index = 0; $index < 30; $index++) {
+            $token .= $tab[rand(0, $max)];
         }
         return $token;
     }
