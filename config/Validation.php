e588d9a73d0af94dc519185f89794ed1ee46aba0
NilsFradin <nilsfradin@hotmail.fr>
2017-12-04 10:36:20 +0100

    change on validation

--- config/Validation.php
+++ config/Validation.php
@@ -1,19 +1,23 @@
 <?php
+
 class Validation {
-     public static function cleanEmail ($value){
+
+    public static function cleanEmail($value) {
         return filter_var($value, FILTER_SANITIZE_EMAIL);
     }
-    
-    public static function cleanString ($value){
+
+    public static function cleanString($value) {
         return filter_var($value, FILTER_SANITIZE_STRING);
     }
-    
-    public static function cleanInt ($value){
+
+    public static function cleanInt($value) {
         return filter_var($value, FILTER_SANITIZE_NUMBER_INT);
     }
-    
-    public static function cleanFloat ($value){
+
+    public static function cleanFloat($value) {
         return filter_var($value, FILTER_SANITIZE_NUMBER_FLOAT);
     }
+
 }
+
 ?>
