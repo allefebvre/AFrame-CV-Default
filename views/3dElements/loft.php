2e3fbf59b3babf323229c8cb3c8903b42276aa65
Alexandre LEFEBVRE <Alexandre.LEFEBVRE@etu.uca.fr>
2017-12-04 10:59:13 +0100

    improve presentation loft.php

--- views/3dElements/loft.php
+++ views/3dElements/loft.php
@@ -1,11 +1,17 @@
 <?php
 // Loading of the camera position from the GET and the COOKIES (GET first) //
 
+$default_posX = -10;
+$default_posY = 0;
+$default_posZ = 0;
+$default_rotationX = 10;
+$default_rotationY = 90;
+
 $posX = filter_input(INPUT_GET, 'posX', FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
 if ($posX == NULL || $posX == false) {
     $posX = filter_input(INPUT_COOKIE, 'posX', FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
     if ($posX == NULL || $posX == false || $posX > 19.5 || $posX < -19.5) {
-        $posX = -10;
+        $posX = $default_posX;
     }
 }
 
@@ -13,7 +19,7 @@ $posY = filter_input(INPUT_GET, 'posY', FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLA
 if ($posY == NULL || $posY == false) {
     $posY = filter_input(INPUT_COOKIE, 'posY', FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
     if ($posY == NULL || $posY == false || $posY > 10.5 || $posY < 0) {
-        $posY = 0;
+        $posY = $default_posY;
     }
 }
 
@@ -21,7 +27,7 @@ $posZ = filter_input(INPUT_GET, 'posZ', FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLA
 if ($posZ == NULL || $posZ == false) {
     $posZ = filter_input(INPUT_COOKIE, 'posZ', FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
     if ($posZ == NULL || $posZ == false || $posZ > 14.5 || $posZ < -14.5) {
-        $posZ = 0;
+        $posZ = $default_posZ;
     }
 }
 
@@ -29,7 +35,7 @@ $rotationX = filter_input(INPUT_GET, 'rotationX', FILTER_SANITIZE_NUMBER_FLOAT, 
 if ($rotationX == NULL || $rotationX == false) {
     $rotationX = filter_input(INPUT_COOKIE, 'rotationX', FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
     if ($rotationX == NULL || $rotationX == false) {
-        $rotationX = 10;
+        $rotationX = $default_rotationX;
     }
 }
 
@@ -37,7 +43,7 @@ $rotationY = filter_input(INPUT_GET, 'rotationY', FILTER_SANITIZE_NUMBER_FLOAT, 
 if ($rotationY == NULL || $rotationY == false) {
     $rotationY = filter_input(INPUT_COOKIE, 'rotationY', FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
     if ($rotationY == NULL || $rotationY == false) {
-        $rotationY = 90;
+        $rotationY = $default_rotationY;
     }
 }
 
@@ -54,34 +60,25 @@ if (!isset($light)) {
 }
 ?>
 
-
-
 <!-- Camera settings-->
 <a-entity camera="userHeight: 1.6"
           persistence-position
-          position="<?php echo $posX . " " . $posY . " " . $posZ ?>"
-          rotation="<?php echo $rotationX; ?> <?php echo $rotationY; ?> 0"
+          position="<?php echo "$posX $posY $posZ"; ?>"
+          rotation="<?php echo "$rotationX $rotationY 0"; ?>"
           jump-ability
           border-position
           height-correction
           universal-controls
-          kinematic-body
->
+          kinematic-body>
 
-    <a-entity cursor="fuse: false; fuseTimeout: 500; downEvents: triggerdown; upEvents: triggerup"
+    <a-entity cursor="fuseTimeout: 500; downEvents: triggerdown; upEvents: triggerup"
               position="0 0 -1"
               raycaster="far:10"
               geometry="primitive: ring; radiusInner: 0.01; radiusOuter: 0.015"
               material="color: black; shader: flat">
-        <a-entity vive-controls="hand: left; model:false"></a-entity>
-        <a-entity vive-controls="hand: right; model:false"></a-entity>
     </a-entity>
+    
 </a-entity>
-
-
-<!--<a-entity laser-controls="hand: left"></a-entity>
-<a-entity laser-controls="hand: right"></a-entity>-->
-
 <!-- End camera settings -->
 
 <!-- Loading textures -->
@@ -95,7 +92,6 @@ if (!isset($light)) {
     <img id="panneau" src="resources/textures/panneau.png"/>
 </a-assets>
 <!-- End loading textures -->
-
 
 <!-- Forest environment -->
 <a-entity position="0 -1 0" environment="preset: forest; lightPosition: 0 0 0; lighting: non" scale="1.2 1.2 1.2"></a-entity>
@@ -140,12 +136,10 @@ if (!isset($light)) {
 <a-box src="#wallTexture" mixin="wall" static-body position="-19.5 5 0" scale="0.25 10 11" ></a-box>
 <a-box src="#wallTexture" mixin="wall" static-body position="-19.5 5 14" scale="0.25 10 2" ></a-box>
 <a-box src="#wallTexture" mixin="wall" static-body position="-19.5 5 -14" scale="0.25 10 2" ></a-box>
-
 <a-entity position="-19.5 5 9.25">
     <a-box src="#woodTexture" mixin="wall" static-body scale="0.15 10 0.25" color="#995522"></a-box>
     <a-box static-body scale="0.1 10 7.5" material="opacity:0.3;color:#b1d9e7" ></a-box>
 </a-entity>
-
 <a-entity position="-19.5 5 -9.25">
     <a-box src="#woodTexture" mixin="wall" static-body scale="0.15 10 0.25" color="#995522"></a-box>
     <a-box static-body scale="0.1 10 7.5" material="opacity:0.3;color:#b1d9e7"></a-box>
@@ -156,12 +150,10 @@ if (!isset($light)) {
 <a-box src="#wallTexture" mixin="wall" static-body position="19.5 5 0" scale="0.25 10 11" ></a-box>
 <a-box src="#wallTexture" mixin="wall" static-body position="19.5 5 14" scale="0.25 10 2" material=""></a-box>
 <a-box src="#wallTexture" mixin="wall" static-body position="19.5 5 -14" scale="0.25 10 2" material=""></a-box>
-
 <a-entity position="19.5 5 9.25">
     <a-box src="#woodTexture" mixin="wall" static-body scale="0.15 10 0.25" color="#995522"></a-box>
     <a-box static-body scale="0.1 10 7.5" material="opacity:0.3;color:#b1d9e7" ></a-box>
 </a-entity>
-
 <a-entity position="19.5 5 -9.25">
     <a-box src="#woodTexture" mixin="wall" static-body scale="0.15 10 0.25" color="#995522"></a-box>
     <a-box static-body scale="0.1 10 7.5" material="opacity:0.3;color:#b1d9e7"></a-box>
@@ -172,12 +164,10 @@ if (!isset($light)) {
 <a-box src="#wallTexture" mixin="wall" static-body position="0 5 14.5"  scale="0.25 10 21" rotation="0 90 0"></a-box>
 <a-box src="#wallTexture" mixin="wall" static-body position="19 5 14.5" scale="0.25 10 2" rotation="0 90 0"></a-box>
 <a-box src="#wallTexture" mixin="wall" static-body position="-19 5 14.5" scale="0.25 10 2" rotation="0 90 0"></a-box>
-
 <a-entity position="14.25 5 14.5" rotation="0 90 0">
     <a-box src="#woodTexture" mixin="wall" static-body scale="0.15 10 0.25" color="#995522"></a-box>
     <a-box static-body scale="0.1 10 7.5" material="opacity:0.3;color:#b1d9e7" ></a-box>
 </a-entity>
-
 <a-entity position="-14.25 5 14.5" rotation="0 90 0">
     <a-box src="#woodTexture" mixin="wall" static-body scale="0.15 10 0.25" color="#995522"></a-box>
     <a-box static-body scale="0.1 10 7.5" material="opacity:0.3;color:#b1d9e7"></a-box>
@@ -186,32 +176,29 @@ if (!isset($light)) {
 
 <!-- 4th wall -->
 <!--<a-box src="#wallTexture" mixin="wall" static-body position="0 5 -14.5"  scale="0.25 10 21" rotation="0 90 0"></a-box>-->
-<a-box src="#wallTexture" mixin="wall" static-body position="0 2.5 -14.5" scale="0.25 5 21" rotation="0 90 0"></a-box>
-<a-box src="#wallTexture" mixin="wall" static-body position="7.5 7.5 -14.5" scale="0.25 5 6" rotation="0 90 0"></a-box>
-<a-box src="#wallTexture" mixin="wall" static-body position="-9.4 7.5 -14.5" scale="0.25 5 2.8" rotation="0 90 0"></a-box>
-<a-box src="#wallTexture" mixin="wall" static-body position="-1.753 5.065 -15.5" scale="0.25 13.228 2" rotation="0 0 90"></a-box>
-<a-box static-body position="4.719 7.554 -15.492" scale="0.25 5 2.022" material="visible:false"></a-box>
-<a-box static-body position="-8.2 7.554 -15.492" scale="0.25 5 2.022" material="visible:false"></a-box>
-<a-box static-body position="-1.775 7.55 -16" scale="0.25 5 13.51" rotation="0 90 0" material="visible:false"></a-box>
-<a-box static-body position="-1.775 10.033 -15.482" scale="0.25 1.726 13.51" rotation="0 90 90" material="visible:false"></a-box>
-<a-box src="#wallTexture" mixin="wall" static-body position="-1.753 9.5 -14.5" scale="0.24 13.228 1.045" rotation="90 0 90"></a-box>
-<a-box static-body src="#woodTexture" mixin="wall" position="-1.753 6.5 -16.473" scale="0.07 0.15 13.081" color="#e1a975" rotation="0 90 0"></a-box>
-<a-box static-body src="#woodTexture" mixin="wall" position="4.75 6.5 -15.54" scale="0.07 0.15 1.936" color="#e1a975"></a-box>
-<a-box static-body src="#woodTexture" mixin="wall" position="-8.29 6.5 -15.54" scale="0.07 0.15 1.936" color="#e1a975"></a-box>
+<a-box static-body src="#wallTexture" mixin="wall"  position="0 2.5 -14.5"              scale="0.25 5 21"           rotation="0 90 0"></a-box>
+<a-box static-body src="#wallTexture" mixin="wall"  position="7.5 7.5 -14.5"            scale="0.25 5 6"            rotation="0 90 0"></a-box>
+<a-box static-body src="#wallTexture" mixin="wall"  position="-9.4 7.5 -14.5"           scale="0.25 5 2.8"          rotation="0 90 0"></a-box>
+<a-box static-body src="#wallTexture" mixin="wall"  position="-1.753 5.065 -15.5"       scale="0.25 13.228 2"       rotation="0 0 90"></a-box>
+<a-box static-body material="visible:false"         position="4.719 7.554 -15.492"      scale="0.25 5 2.022"></a-box>
+<a-box static-body material="visible:false"         position="-8.2 7.554 -15.492"       scale="0.25 5 2.022"></a-box>
+<a-box static-body material="visible:false"         position="-1.775 7.55 -16"          scale="0.25 5 13.51"        rotation="0 90 0"></a-box>
+<a-box static-body material="visible:false"         position="-1.775 10.033 -15.482"    scale="0.25 1.726 13.51"    rotation="0 90 90"></a-box>
+<a-box static-body src="#wallTexture" mixin="wall"  position="-1.753 9.5 -14.5"         scale="0.24 13.228 1.045"   rotation="90 0 90"></a-box>
+<a-box static-body src="#woodTexture" mixin="wall"  position="-1.753 6.5 -16.473"       scale="0.07 0.15 13.081"    rotation="0 90 0" color="#e1a975"></a-box>
+<a-box static-body src="#woodTexture" mixin="wall"  position="4.75 6.5 -15.54"          scale="0.07 0.15 1.936" color="#e1a975"></a-box>
+<a-box static-body src="#woodTexture" mixin="wall"  position="-8.29 6.5 -15.54"         scale="0.07 0.15 1.936" color="#e1a975"></a-box>
 <?php
 fence(20, -8, 4.4, -16.473, 5.750, TRUE);
 fence(2, -16.2, -14.9, 4.75, 5.750);
 fence(2, -16.2, -14.9, -8.29, 5.750);
 ?>
-
-<a-box src="#wallTexture" mixin="wall" static-body position="19 5 -14.5" scale="0.25 10 2" rotation="0 90 0"></a-box>
+<a-box src="#wallTexture" mixin="wall" static-body position="19 5 -14.5"  scale="0.25 10 2" rotation="0 90 0"></a-box>
 <a-box src="#wallTexture" mixin="wall" static-body position="-19 5 -14.5" scale="0.25 10 2" rotation="0 90 0"></a-box>
-
 <a-entity position="14.25 5 -14.5" rotation="0 90 0">
     <a-box src="#woodTexture" mixin="wall" static-body scale="0.15 10 0.25" color="#995522"></a-box>
     <a-box static-body scale="0.1 10 7.5" material="opacity:0.3;color:#b1d9e7" ></a-box>
 </a-entity>
-
 <a-entity position="14.25 5 -14.5" rotation="0 90 0">
     <a-box src="#woodTexture" mixin="wall" static-body scale="0.15 10 0.25" color="#995522"></a-box>
     <a-box static-body scale="0.1 10 7.5" material="opacity:0.3;color:#b1d9e7"></a-box>
@@ -224,22 +211,22 @@ fence(2, -16.2, -14.9, -8.29, 5.750);
 <a-box static-body src="#wallTexture" mixin="wall" position="0 5 0" scale="20.99 0.4 29.2"></a-box>
 
 <!-- Access ramp on the 1st floor -->
-<a-box static-body src="#wallTexture" mixin="wall" position="12.5 5 8" scale="4 0.4 6"></a-box>
+<a-box static-body src="#wallTexture" mixin="wall" position="12.5 5 8"          scale="4 0.4 6"></a-box>
 <a-box static-body src="#wallTexture" mixin="wall" position="12.5 2.292 -0.350" scale="4 0.4 12.171" rotation="-26.699833253096365 0 0"></a-box>
 
-<a-box static-body src="#wallTexture" position="12.5 1.2 1.161" scale="4 3.176 0.602" material="visible:false"></a-box>
-<a-box static-body src="#wallTexture" position="12.5 0.395 -0.681" scale="4 1.822 3.084" material="visible:false"></a-box>
-<a-box static-body src="#wallTexture" position="10.428 4 1.793" scale="0.129 1.822 3.789" material="visible:false"></a-box>
+<a-box static-body src="#wallTexture" position="12.5 1.2 1.161"     scale="4 3.176 0.602"       material="visible:false"></a-box>
+<a-box static-body src="#wallTexture" position="12.5 0.395 -0.681"  scale="4 1.822 3.084"       material="visible:false"></a-box>
+<a-box static-body src="#wallTexture" position="10.428 4 1.793"     scale="0.129 1.822 3.789"   material="visible:false"></a-box>
 <!-- End access ramp on the 1st floor -->
 
 <!-- Fence -->
-<a-box static-body src="#woodTexture" mixin="wall" position="-10.35 6.5 0" scale="0.07 0.15 29.2" color="#e1a975"></a-box>
-<a-box static-body src="#woodTexture" mixin="wall" position="10.35 6.5 -4.458" scale="0.07 0.15 19.86" color="#e1a975"></a-box>
-<a-box static-body src="#woodTexture" mixin="wall" position="10.35 5.75 5.284" scale="0.069 1.5 0.15" color="#e1a975"></a-box>
-<a-box static-body src="#woodTexture" mixin="wall" position="10.35 6.5 12.694" scale="0.07 0.15 3.482" color="#e1a975"></a-box>
-<a-box static-body src="#woodTexture" mixin="wall" position="12.355 6.5 10.919" scale="0.07 0.15 4.08" color="#e1a975" rotation="0 90 0"></a-box>
-<a-box static-body src="#woodTexture" mixin="wall" position="14.379 6.5 8.14" scale="0.07 0.15 5.63" color="#e1a975"></a-box>
-<a-box static-body src="#woodTexture" mixin="wall" position="14.379 5.75 5.5" scale="0.069 1.5 0.15" color="#e1a975"></a-box>
+<a-box static-body src="#woodTexture" mixin="wall" position="-10.35 6.5 0"      scale="0.07 0.15 29.2"  color="#e1a975"></a-box>
+<a-box static-body src="#woodTexture" mixin="wall" position="10.35 6.5 -4.458"  scale="0.07 0.15 19.86" color="#e1a975"></a-box>
+<a-box static-body src="#woodTexture" mixin="wall" position="10.35 5.75 5.284"  scale="0.069 1.5 0.15"  color="#e1a975"></a-box>
+<a-box static-body src="#woodTexture" mixin="wall" position="10.35 6.5 12.694"  scale="0.07 0.15 3.482" color="#e1a975"></a-box>
+<a-box static-body src="#woodTexture" mixin="wall" position="12.355 6.5 10.919" scale="0.07 0.15 4.08"  color="#e1a975" rotation="0 90 0"></a-box>
+<a-box static-body src="#woodTexture" mixin="wall" position="14.379 6.5 8.14"   scale="0.07 0.15 5.63"  color="#e1a975"></a-box>
+<a-box static-body src="#woodTexture" mixin="wall" position="14.379 5.75 5.5"   scale="0.069 1.5 0.15"  color="#e1a975"></a-box>
 
 <?php
 
@@ -354,7 +341,7 @@ function spot(float $x, float $y, float $z, int $rotation) {
             <a-plane material="side:double" color="#111" scale="0.15 0.2 1"></a-plane>
         </a-entity>
     </a-entity>
-<?php
+    <?php
 }
 
 if ($spotlight) {
@@ -384,4 +371,5 @@ if ($spotlight) {
     <a-entity obj-model="obj:resources/model3D/Canape.obj" position="-16.61 -0.01 5.639" scale="2 2 2"></a-entity>
     <a-entity static-body obj-model="mtl:resources/model3D/house plant.mtl;obj:resources/model3D/house plant.obj" position="-18.513 -0.01 7.619" scale="0.002 0.002 0.002"></a-entity>
     <a-entity obj-model="mtl:resources/model3D/Pendule.mtl;obj:resources/model3D/Pendule.obj" position="16.436 3.62 -8" scale="0.02 0.02 0.02" rotation="0 -90 0"></a-entity>
-<?php }
+<?php
+}
