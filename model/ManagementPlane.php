53550312fa1aaee9cfddf431f30f7fa3aa3fccdd
syescassut <Sylvain.ESCASSUT@etu.uca.fr>
2017-12-12 14:45:01 +0100

    fix bug

--- model/ManagementPlane.php
+++ model/ManagementPlane.php
@@ -27,8 +27,10 @@ class ManagementPlane {
      */
     public function placeHTML(array $data) {
         for ($index=0 ; $index<count($this->listPLane) ; $index++) {
-            $content_en = $data['rubriques'][$index]->getContent_en();
-            $targetId = $this->listPLane[$index]->getTargetId();
+            if(isset($data['rubriques'][$index])) {
+                $content_en = $data['rubriques'][$index]->getContent_en();
+                $targetId = $this->listPLane[$index]->getTargetId();
+            }
             echo "<div class=\"hide\">";
             require $this->listPLane[$index]->getPathHTML();
             echo "</div>";
